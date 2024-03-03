e1be61b0d39e1175b317bc9d39f0d2a24b09e4fe
adrianmaciuc <adrian.maciuc@gmail.com>
2024-03-03 13:45:43 +0200

    have tests ready

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,11 +21,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    }
   ],
 });
