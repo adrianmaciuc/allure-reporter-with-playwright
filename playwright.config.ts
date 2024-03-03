80cddd54c22a260f2cea49740698d14ef263bf97
adrianmaciuc <adrian.maciuc@gmail.com>
2024-03-03 13:30:32 +0200

    initial phase

---
+++ playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from '@playwright/test';
+
+require('dotenv').config();
+
+
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 2 : 2,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'allure-playwright',
+  use: {
+    // website under test uses id attributes instead of default data-testid 
+    testIdAttribute: 'id',
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
