f615be7029246dc3bcc11b13357743a85d327892
adrianmaciuc <adrian.maciuc@gmail.com>
2024-03-05 19:43:32 +0200

    update comments and cleanup config

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,15 +4,9 @@ export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
-  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 3 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'allure-playwright',
-  use: {
-    // website under test uses id attributes instead of default data-testid 
-    testIdAttribute: 'id',
-    trace: 'on-first-retry',
-  },
 
   projects: [
     {
