1157971bcf621331be59438272ef0a009df6f690
adrianmaciuc <adrian.maciuc@gmail.com>
2024-03-27 21:13:15 +0200

    update tests

--- tests/parallel.spec.ts
+++ tests/parallel.spec.ts
@@ -10,6 +10,7 @@ test.describe('full PARALLEL mode', () => {
   test("2 parallel", async ({ page }, testInfo) => {
     console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
   });
+
   test("3 parallel", async ({ page }, testInfo) => {
     console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
   });
@@ -18,19 +19,4 @@ test.describe('full PARALLEL mode', () => {
     console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
   });
 
-  test("5 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  });
-
-  test("6 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  });
-
-  test("7 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  })
-
-  test("8 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  });
 });
