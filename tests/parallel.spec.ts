383b04753d8cee6c2d5242708e9c9a281a0e08c6
adrianmaciuc <adrian.maciuc@gmail.com>
2024-03-27 21:29:28 +0200

    set to true full paralel

--- tests/parallel.spec.ts
+++ tests/parallel.spec.ts
@@ -19,20 +19,4 @@ test.describe('full PARALLEL mode', () => {
     console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
   });
 
-  test("5 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  });
-
-  test("6 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  });
-
-  test("7 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  });
-
-  test("8 parallel", async ({ page }, testInfo) => {
-    console.log(`test name: ${testInfo.title} | parallel index :${testInfo.parallelIndex} | shard index: ${JSON.stringify(testInfo.config.shard)}`)
-  });
-
 });
