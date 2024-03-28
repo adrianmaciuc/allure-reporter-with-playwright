31f10811a82b23def86e7d5643828dc4f148f885
adrianmaciuc <adrian.maciuc@gmail.com>
2024-03-28 21:09:34 +0200

    add one more serial

--- tests/serial.spec.ts
+++ tests/serial.spec.ts
@@ -6,15 +6,19 @@ test.describe.configure({ mode: 'serial' });
 test.describe('SERIAL suite', () => {
 
   test("1 serial", async ({ page }, testInfo) => {
-    console.log(`spec.file.name: ${testInfo.titlePath[0]} | test.suite: ${testInfo.titlePath[1]} | test.name: ${testInfo.title} | worker_id :${testInfo.parallelIndex} | shard.index: ${JSON.stringify(testInfo.config.shard)}`)
+    console.log(`file.name: ${testInfo.titlePath[0]} | describe: ${testInfo.titlePath[1]} | test.name: ${testInfo.title} | worker_id :${testInfo.parallelIndex} | shard.index: ${JSON.stringify(testInfo.config.shard)}`)
   });
 
   test("2 serial", async ({ page }, testInfo) => {
-    console.log(`spec.file.name: ${testInfo.titlePath[0]} | test.suite: ${testInfo.titlePath[1]} | test.name: ${testInfo.title} | worker_id :${testInfo.parallelIndex} | shard.index: ${JSON.stringify(testInfo.config.shard)}`)
+    console.log(`file.name: ${testInfo.titlePath[0]} | describe: ${testInfo.titlePath[1]} | test.name: ${testInfo.title} | worker_id :${testInfo.parallelIndex} | shard.index: ${JSON.stringify(testInfo.config.shard)}`)
   });
 
   test("3 serial", async ({ page }, testInfo) => {
-    console.log(`spec.file.name: ${testInfo.titlePath[0]} | test.suite: ${testInfo.titlePath[1]} | test.name: ${testInfo.title} | worker_id :${testInfo.parallelIndex} | shard.index: ${JSON.stringify(testInfo.config.shard)}`)
+    console.log(`file.name: ${testInfo.titlePath[0]} | describe: ${testInfo.titlePath[1]} | test.name: ${testInfo.title} | worker_id :${testInfo.parallelIndex} | shard.index: ${JSON.stringify(testInfo.config.shard)}`)
+  });
+
+  test("4 serial", async ({ page }, testInfo) => {
+    console.log(`file.name: ${testInfo.titlePath[0]} | describe: ${testInfo.titlePath[1]} | test.name: ${testInfo.title} | worker_id :${testInfo.parallelIndex} | shard.index: ${JSON.stringify(testInfo.config.shard)}`)
   });
 
 });
