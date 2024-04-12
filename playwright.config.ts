a82b73c44dfcbe54da4e8569f593c06c3479314b
adrianmaciuc <adrian.maciuc@gmail.com>
2024-04-12 19:14:20 +0300

    reconfigure back with shards

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,14 +3,14 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
-  workers: process.env.CI ? 3 : 1,
+  fullyParallel: true,
+  workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'allure-playwright',
 
   projects: [
     {
-      name: 'x',
+      name: 'myProject',
       use: { ...devices['Desktop Chrome'] },
     }
   ],
